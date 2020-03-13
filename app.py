e8169440c3f437d6b2c9a1903892736efb157f35
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2020-03-13 19:32:02 -0300

    example of broken code

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form  
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = FastAPI()
+ 
+class User():
+    @staticmethod
+    def get_user_from_phone_number(sender):
+        return User()
 
+    def reset_password(self):
+        return "12345"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post('/sms/reset')
+async def reset(From: str = Form(...)):
+    sender = From
+    user = User.get_user_from_phone_number(sender)
+    temporary_password = user.reset_password()
+    resp = MessagingResponse()
+    msg = resp.message()
+    msg.body(f'Your reset link is https://example.com/reset/{temporary_password}')
+    return str(resp)
